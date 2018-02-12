ae7b2b1337d2d238ff4dfc719272a0e1c4b0dc4e
ED <33886968+EDegg1@users.noreply.github.com>
2018-02-12 14:49:17 +0900

    지원사업 탐색 벨류 추가

--- EGG1/src/main/java/com/xml/XmlTest.java
+++ EGG1/src/main/java/com/xml/XmlTest.java
@@ -10,38 +10,38 @@ import org.jdom2.input.SAXBuilder;
 
 public class XmlTest {
 	public List<Element> asdf(){
-		String url="http://openapi.kised.or.kr/openapi/service/rest/ContentsService/getAnnouncementList?ServiceKey=TAcPXVDYcd5PGo1LfvXTia2fd5pw5eE60IPowYJj%2BZtjFJ8v%2FOu8KWj97T7QoH3WzebCJ3Fow%2BS2gD3%2BpDqRVQ%3D%3D&pageNo=5&numOfRows=50";
-		  //?°?´?„° ?½?–´?“¤?´ê¸?(?ŒŒ?„œ?—­?• )
+		String url="http://openapi.kised.or.kr/openapi/service/rest/ContentsService/getAnnouncementList?ServiceKey=TAcPXVDYcd5PGo1LfvXTia2fd5pw5eE60IPowYJj%2BZtjFJ8v%2FOu8KWj97T7QoH3WzebCJ3Fow%2BS2gD3%2BpDqRVQ%3D%3D&pageNo=2&numOfRows=50";
+		  //?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½ ?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½ï¿½?(?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½)
 		  SAXBuilder builder = new SAXBuilder();
-		  //ë©”ëª¨ë¦¬ì— documentë¥? ë§Œë“ ?‹¤, ?Š¤?Š¸ë¦¼ì„ ?“¸ê²½ìš° ë©”ì„¸ì§??— ?”°?¼ ì²˜ë¦¬?•´ì£¼ë©´?œ?‹¤
+		  //ë©”ëª¨ë¦¬ì— documentï¿½? ë§Œë“ ?ï¿½ï¿½, ?ï¿½ï¿½?ï¿½ï¿½ë¦¼ì„ ?ï¿½ï¿½ê²½ìš° ë©”ì„¸ï¿½??ï¿½ï¿½ ?ï¿½ï¿½?ï¿½ï¿½ ì²˜ë¦¬?ï¿½ï¿½ì£¼ë©´?ï¿½ï¿½?ï¿½ï¿½
 		  Document jdomdoc;
 		  List<Element> lis_E = null;
 		  try {
 			   jdomdoc = builder.build(new URL(url));
 			   System.out.println("ë£¨íŠ¸:"+jdomdoc.toString());
 			   
-			   //rootë¥? êº¼ëƒ„(Sample.xml?˜ Family)
+			   //rootï¿½? êº¼ëƒ„(Sample.xml?ï¿½ï¿½ Family)
 			   Element root = jdomdoc.getRootElement();
 			   System.out.println("ë£¨íŠ¸ ë§ë‚˜?"+root);
 			   
-			   //root?˜ ??‹?„ êº¼ëƒ„.
+			   //root?ï¿½ï¿½ ?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½ êº¼ëƒ„.
 			   Element desc_E = root.getChild("body");
-			   System.out.println("??‹?„?"+desc_E);
+			   System.out.println("?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½?"+desc_E);
 			   String description = desc_E.getValue();
 			   System.out.println("description="+description);
 			   
 			   
-			   //ë°˜ë³µ? ?¸ ?°?´?„°?Š” ë°˜ë³µë¬¸ìœ¼ë¡? êº¼ë‚´?¼
+			   //ë°˜ë³µ?ï¿½ï¿½?ï¿½ï¿½ ?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½ ë°˜ë³µë¬¸ìœ¼ï¿½? êº¼ë‚´?ï¿½ï¿½
 			   Element desc_P = root.getChild("body");
 			   Element it = desc_P.getChild("items");
-			   System.out.println("body?„??"+desc_P);
-			   System.out.println("body?˜ ??‹??"+desc_P.getChildren("items"));
+			   System.out.println("body?ï¿½ï¿½??"+desc_P);
+			   System.out.println("body?ï¿½ï¿½ ?ï¿½ï¿½?ï¿½ï¿½??"+desc_P.getChildren("items"));
 			  //List<Element> lis_E = desc_P.getChildren("items");
 			   lis_E = it.getChildren("item");
 			   
 			   for(int i=0; i<lis_E.size(); i++){
 				   Element person_E=lis_E.get(i);
-			    System.out.println("item?¸ê°???"+person_E);
+			    System.out.println("item?ï¿½ï¿½ï¿½???"+person_E);
 			   // System.out.println("ssn="+person_E.getAttributeValue("item"));
 			    System.out.println("firstName="+person_E.getValue());
 			    //System.out.println("lastName="+person_E.getChild("detailurl").getValue());
--- EGG1/src/main/webapp/WEB-INF/view/searchSupport.jsp
+++ EGG1/src/main/webapp/WEB-INF/view/searchSupport.jsp
@@ -13,6 +13,11 @@
 				<option value="창업교육">창업</option>
 				<option value="정책자금">자금</option>
 				<option value="판로·해외진출">판로</option>
+				<option value="멘토링·컨설팅">멘토링·컨설팅</option>
+				<option value="사업화">사업화</option>
+				<option value="시설·공간">시설·공간</option>
+				<option value="R&D">R&D</option>
+				<option value="행사·네트워크">행사·네트워크</option>
 				<!-- 
 				<option value="인력">인력</option>
 				<option value="연구">연구</option>
@@ -23,11 +28,16 @@
 			</select>
 			<select name="target">
 				<option value="일반기업">일반기업</option>
-				<option value="중소기업">중소기업</option>
-				<option value="소상공인">소상공인</option>
-				<option value="1인기업">1인기업</option>
-				<option value="창업기업">창업기업</option>
-				<option value="예비창업자">예비창업자</option>
+<!-- 				<option value="중소기업">중소기업</option> -->
+<!-- 				<option value="소상공인">소상공인</option> -->
+				<option value="1인 창조기업">1인 창조기업</option>
+<!-- 				<option value="창업기업">창업기업</option> -->
+<!-- 				<option value="예비창업자">예비창업자</option> -->
+				<option value="대학생">대학생</option>
+				<option value="일반인">일반인</option>
+				<option value="대학">대학</option>
+				<option value="연구기관">연구기관</option>
+				<option value="청소년">청소년</option>
 				<option value="기타">기타</option>
 			</select>
 			<select name="area">
@@ -39,6 +49,9 @@
 				<option value="광주광역시">광주광역시</option>
 				<option value="대전광역시">대전광역시</option>
 				<option value="대구광역시">대구광역시</option>
+				<option value="부산광역시">부산광역시</option>
+				<option value="충청남도">충청남도</option>
+				<option value="충청북도">충청북도</option>
 			</select>
 			<input type="submit" value="검색">
 		</form>
