2fa90a5395883dc5ac251ea0aa422a8d59d1fff2
walker <wk@egg1.site>
2018-02-24 17:11:17 +0900

    Revert "마이페이지쪽"
    This reverts commit e6049c3a69cf1127c48944350f3fa7b8db03b6a1.

--- TestReservation/src/main/java/com/hk/lab5/model/IDao.java
+++ TestReservation/src/main/java/com/hk/lab5/model/IDao.java
@@ -1,13 +1,8 @@
 package com.hk.lab5.model;
 
-import java.util.List;
 import java.util.Map;
 
 import com.hk.lab5.dtos.AccountDto;
-import com.hk.lab5.dtos.MatchingDto;
-import com.hk.lab5.dtos.QuestDto;
-import com.hk.lab5.dtos.ReportDto;
-import com.hk.lab5.dtos.WalletlogDto;
 
 public interface IDao {
 
@@ -18,16 +13,5 @@ public interface IDao {
 	boolean coupon(Map<String, String> map);
 	boolean couponChk(String code);
 	void couponUse(Map<String, String> map);
-	void insertCouponLog(Map<String, String> map);
-	boolean changeInfo(Map<String, String> map);
-	List<WalletlogDto> selectWlog(String id);
-	Integer makeQCount(String id);
-	Integer tryQCount(String id);
-	Integer matchingQCount(String id);
-	Integer reportCount(String id);
-	List<QuestDto> makeQList(String id);
-	List<QuestDto> tryQList(String id);
-	List<MatchingDto> matchingQList(String id);
-	List<ReportDto> reportList(String id);
 	
 }
